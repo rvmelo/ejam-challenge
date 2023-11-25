fea3c969c74f17fe3e11e03094c835e8592eb429
rvmelo <robertomelo7@hotmail.com>
2023-11-25 06:54:32 -0300

    feat: add logo container

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { Header } from '../../components/header'
+import { LogoContainer } from './styles'
+import clarifonLogo from '../../assets/clarifon-logo.svg'
+import softwareLogo from '../../assets/software-logo.svg'
 
 export const Home: React.FC = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <LogoContainer>
+        <img src={clarifonLogo} alt="" />
+        <img src={softwareLogo} alt="" />
+      </LogoContainer>
+    </>
+  )
 }
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1.875rem 7.9375rem;
+`
