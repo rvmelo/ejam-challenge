1e8c6393d60e05d9757cad50ebdf72f89e91943a
rvmelo <robertomelo7@hotmail.com>
2023-11-26 21:38:43 -0300

    refactor: some changes

--- src/components/purchaseInfo/index.tsx
+++ src/components/purchaseInfo/index.tsx
@@ -6,12 +6,26 @@ import creditCardsLogo from '../../assets/credit-cards.svg'
 export const PurchaseInfo: React.FC = () => {
   return (
     <PurchaseContainer>
-      <span>Free shipping</span>
-      <div className="small-line" />
-      <img className="lockImg" src={lockLogo} alt="" />
-      <span>Secure 256-bit SSL encryption.</span>
-      <div className="small-line" />
-      <img src={creditCardsLogo} alt="" />
+      <div className="regular">
+        <span>Free shipping</span>
+        <div className="small-line" />
+        <img className="lockImg" src={lockLogo} alt="" />
+        <span>Secure 256-bit SSL encryption.</span>
+        <div className="small-line" />
+        <img src={creditCardsLogo} alt="" />
+      </div>
+      <div className="mobile">
+        <div className="first-item">
+          <span>Free shipping</span>
+          <div className="small-line" />
+          <img className="lockImg" src={lockLogo} alt="" />
+          <span>Secure 256-bit SSL encryption.</span>
+        </div>
+        <div className="second-item">
+          <div className="small-line-2" />
+          <img src={creditCardsLogo} alt="" />
+        </div>
+      </div>
     </PurchaseContainer>
   )
 }
--- src/components/purchaseInfo/styles.ts
+++ src/components/purchaseInfo/styles.ts
@@ -1,16 +1,22 @@
 import styled from 'styled-components'
 
 export const PurchaseContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  border: 1px solid #cfcfcf;
-  border-radius: 4px;
-  padding: 0.5rem 1rem;
+  .regular {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    border: 1px solid #cfcfcf;
+    border-radius: 4px;
+    padding: 0.5rem 1rem;
 
-  margin-bottom: 1.25rem;
+    margin-bottom: 1.25rem;
+
+    @media (max-width: 430px) {
+      display: none;
+    }
+  }
 
   span {
     color: ${({ theme }) => theme.gray};
@@ -27,5 +33,54 @@ export const PurchaseContainer = styled.div`
 
   .lockImg {
     margin-right: 0.625rem;
+  }
+
+  .mobile {
+    @media (min-width: 430px) {
+      display: none;
+    }
+
+    margin: 0.75rem 0;
+    align-self: center;
+    height: 4.4rem;
+
+    @media (max-width: 430px) {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      border: 1px solid #cfcfcf;
+      border-radius: 4px;
+
+      .lockImg {
+        margin-right: 8px;
+      }
+
+      .first-item {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0.5rem 1rem;
+      }
+
+      .second-item {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 0 1rem;
+      }
+
+      .small-line {
+        margin: 0 0.75rem;
+      }
+
+      .small-line-2 {
+        border-top: 1px solid #cfcfcf;
+        height: 0.25px;
+        width: 100%;
+        margin-bottom: 0.75rem;
+      }
+    }
   }
 `
